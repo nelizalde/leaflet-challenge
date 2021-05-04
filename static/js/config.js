ab8ea60e3738d44a35288ca0f136585c1480cf3b
Naomi <n_elizalde@u.pacific.edu>
2021-05-03 19:42:05 -0700

    updated config files

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,3 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+// API key pulled from Activites 
+const API_KEY = "pk.eyJ1IjoiZWxpemFsZGVuYW9taSIsImEiOiJja24xem1lbGQxMnB3Mm5ubG5kZ3FsMDY3In0.b0Sr0h3CGTs5bX3u6X664Q";
